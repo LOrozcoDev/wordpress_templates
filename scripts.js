c17bc36f2e108c8b6097dada1719fe574affc18f
LOrozcoDev <devorozcol@gmail.com>
2025-06-28 21:44:09 -0400

    Updates

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ fetch('proyectos.json')
         const grid = document.getElementById('projectGrid');
         data.forEach(project => {
             const card = document.createElement('div');
-            card.className = 'bg-white p-4 rounded-lg shadow border';
+            card.className = ' bg-dark-bg  dark:bg-white bg-opacity-10 dark:bg-opacity-20 backdrop-blur-[10px] rounded-md border-2 border-dark-bg dark:border-white border-opacity-30 dark:shadow-glass-dark shadow-glass-light  transition-transform hover:scale-105 p-4 rounded-lg shadow border';
 
             card.innerHTML = `
         ${project.thumbnail ? `<img src="${project.thumbnail}" class="w-full h-40 object-cover rounded mb-2" alt="${project.name}">` : ''}
